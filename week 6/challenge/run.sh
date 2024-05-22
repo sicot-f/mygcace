26569b3d89bf50e0729fc14f985444b2fbccc4b4
Frederic Sicot <frederic.sicot@gmail.com>
2024-05-22 09:37:41 +0200

    forgot run.sh

--- week 6/challenge/run.sh
+++ week 6/challenge/run.sh
@@ -1,57 +1,17 @@
 #!/bin/bash
 
-export PROJECT_ID=""
-export REGION=""
-export ZONE=""
-
-# copy variables
-cp variables.tf modules/storage/
-cp variables.tf modules/instances/
+export PROJECT_ID="qwiklabs-gcp-01-6cf129908b08"
+export REGION="us-central1"
+export ZONE="us-central1-b"
 
 # init terraform
 terraform init
 
-terraform import module.instances.google_compute_instance.tf-instance-1 "$PROJECT_ID/$PNE/tf-instance-1"ZO
-terraform import module.instances.google_compute_instance.tf-instance-2 "$PROJECT_ID/$ZPNE/tf-instance-2"
+terraform import "module.gcs-instances.google_compute_instance.tf-instance-1" "$PROJECT_ID/$PNE/tf-instance-1"
+terraform import "module.gcs-instances.google_compute_instance.tf-instance-2" "$PROJECT_ID/$PNE/tf-instance-2"
 
 terraform plan
 terraform apply
 
 # 3
 terraform init -migrate-state
-
-# 6
-module "test-vpc-module" {
-  source       = "terraform-google-modules/network/google"
-  version      = "6.0.0"
-  project_id   = var.project_id # Replace this with your project ID
-  network_name = ""
-  routing_mode = "GLOBAL"
-
-  subnets = [
-    {
-      subnet_name   = "subnet-01"
-      subnet_ip     = "10.10.10.0/24"
-      subnet_region         = var.region
-    },
-    {
-      subnet_name           = "subnet-02"
-      subnet_ip             = "10.10.20.0/24"
-      subnet_region         = var.Reion
-    },
-  ]
-}
-
-# 7
-resource "google_compute_firewall" "default" {
-  name    = "test-firewall"
-  network = "projects/PROJECT_ID/global/networks/VPC Name"
-  source_ranges = "0.0.0.0/0"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
-
-  source_tags = ["web"]
-}
