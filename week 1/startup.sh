6a25f4e49c994fb32a07603b36e8deabb14d0a0e
Frédéric Sicot <frederic.sicot@airbus.com>
2024-08-12 15:43:33 +0200

    test

--- week 1/startup.sh
+++ week 1/startup.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 apt-get update #
-apt-get install -y nginx
+apt-get install -y nginx #
 service nginx start
 sed -i -- 's/nginx/Google Cloud Platform - '"$HOSTNAME"'/' /var/www/html/index.nginx-debian.html
